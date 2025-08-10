7db3a4848ee6bd35ff9a5ccc64b607ff56fd4de8
karamakcsc <karamakcsc@hotmail.com>
2025-08-10 12:29:31 +0300

    update rename

--- masar_royal_gas/override/item.py
+++ masar_royal_gas/override/item.py
@@ -4,39 +4,29 @@ from frappe.model.document import Document
 
 class _Item(Document):
     def autoname(self):
-        # Only apply to base items (not variant, not template)
         if self.variant_of:
-            # frappe.msgprint("Skipping: This item is a variant.")
+            self.name = self.item_code
             return
-
         if self.has_variants:
-            frappe.msgprint("Skipping: This item is a template (has variants).")
+            self.name = self.item_code
             return
-
-        # Skip if item_code already exists
-        if self.item_code:
-            frappe.msgprint("Skipping: item_code already set.")
-            return
-
-        # Build list of item group hierarchy
+        # if self.item_code:
+        #     self.name = self.item_code
+            
         item_groups = []
         current_group = self.item_group
         while current_group:
             if current_group != 'All Item Groups':
                 item_groups.insert(0, current_group)
-            current_group = frappe.db.get_value("Item Group", current_group, "parent_item_group")
-        
-        # Get abbreviations from each group
+            current_group = frappe.db.get_value("Item Group", current_group, "parent_item_group") 
         group_abbrs = []
         for group in item_groups:
             abbr = frappe.db.get_value("Item Group", group, "custom_abbr")
             if not abbr:
                 frappe.throw(_("Abbreviation missing for Item Group: {}").format(group))
             group_abbrs.append(abbr)
-        
-        # Set item_code from abbreviations
         self.item_code = "-".join(group_abbrs).upper().strip()
-        self.item_name = self.item_code
-        self.name = self.item_code
+        self.item_name = "-".join(group_abbrs).upper().strip()
+        self.name = "-".join(group_abbrs).upper().strip()
 
-        frappe.msgprint(f"Item code and name automatically set to: <b>{self.item_code}</b>")
+        frappe.msgprint(f"Item code and name automatically set to: <b>{self.item_code}</b>" , alert = True)
